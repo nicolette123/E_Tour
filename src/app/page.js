5e23065f683e9cb364760528cd5be9cd2866c9d1
isimbinadege <isimbi0123@gmail.com>
2025-06-30 16:14:13 +0300

    implemented login form

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import UserLogin from '@/app/auth/login/UserLogin';
+function Page() {
+    return (
+        <div>
+            
+            <UserLogin />
+        </div>
+    );
+}
+export default Page;
